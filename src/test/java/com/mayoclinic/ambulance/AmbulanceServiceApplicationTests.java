e5c17223ba973eaab03b0f46d3b6f45995009446
sakuna <sakunaxl@gmail.com>
2024-07-13 18:16:26 -0400

    initial commit

---
+++ src/test/java/com/mayoclinic/ambulance/AmbulanceServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mayoclinic.ambulance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AmbulanceServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
